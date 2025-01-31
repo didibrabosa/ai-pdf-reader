a9cf978b2160729c7dbcc64946e8f1b884200c3a
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2025-01-30 21:28:35 -0300

    feat: add essential functions to  the project

--- main.py
+++ main.py
@@ -1,89 +1,66 @@
 import logging
-import os
 import chromadb
 import getpass
+import uuid
+import os
 from langchain_openai import ChatOpenAI
-from langchain.chains import RetrievalQA
-from langchain_community.vectorstores import Chroma
-from flask import Flask, request, jsonify
+from fastapi import FastAPI
+from PyPDF2 import PdfReader
 from dotenv import load_dotenv
-from pdf_reader import read_pdf, extract_text_from_pdf
-from chunking_strategy import invoke_text_spliter
-from chromadb_function import create_collection, add_to_collection
-from langchain.embeddings.openai import OpenAIEmbeddings
 
 logger = logging.getLogger(__name__)
 
 load_dotenv()
 
 if not os.environ.get("OPENAI_API_KEY"):
-    os.environ["OPEN_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+    os.environ["OPENAI_API_KEY"] = getpass.getpass(
+        "Enter API key for OpenAI: ")
 
-app = Flask(__name__)
+app = FastAPI()
 
-chat_model = ChatOpenAI(model="gpt-4", api_key=os.environ["OPENAI_API_KEY"])
+chat_model = ChatOpenAI(
+    model="gpt-4o-mini", api_key=os.environ["OPENAI_API_KEY"])
 
 client = chromadb.PersistentClient("./mycollection")
 
-embedding_function = OpenAIEmbeddings()
+
+def read_pdf(file):
+    return PdfReader(file)
 
 
-def process_pdf_and_query(pdf_filename, collection_name):
-    pdf_path = os.path.join("./pdfs", pdf_filename)
-
-    if not os.path.exists(pdf_path):
-        raise FileNotFoundError(f"The file {pdf_filename} not found.")
-
-    reader = read_pdf(pdf_path)
-    pdf_content = extract_text_from_pdf(reader)
-
-    text_chunks = invoke_text_spliter(
-        separators=["\n\n", "\n", " ", ". ", " "],
-        chunk_size=2000,
-        chunk_overlap=250,
-        content=pdf_content
-    )
-
-    collection = create_collection(collection_name, client)
-    add_to_collection(text_chunks, collection=collection)
-
-    chroma_db = Chroma(
-        persist_directory="./mycollection",
-        embedding_function=embedding_function
-    )
-    chroma_db.add_texts(text_chunks)
-    retriever = chroma_db.as_retriever()
-
-    return retriever
-
-
-@app.route("/read_pdf", methods=["POST"])
-def text_ai():
-    pdf_file = request.files.get("pdf_file")
-    user_question = request.form.get("text")
-
-    if not pdf_file or not user_question:
-        return jsonify({"error": "PDF filename or text not provided"}), 400
-
+def extract_text_from_pdf(reader):
     try:
-        pdf_filename = pdf_file.filename
-        pdf_path = os.path.join("./pdfs", pdf_filename)
+        text = ""
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            text += page.extract_text() or ""
+        return text
+    except Exception as ex:
+        logger.error(f"Error in extract text form pdf: {ex}")
 
-        pdf_file.save(pdf_path)
 
-        retriever = process_pdf_and_query(pdf_filename, collection_name="pdf_collection")
-        qa_chain = RetrievalQA.from_chain_type(
-            llm=chat_model,
-            retriever=retriever,
-            chain_type="stuff"
+def create_collection(collection_name, client):
+    try:
+        collection = client.get_or_create_collection(
+            name=collection_name, metadata={"hnsw:space": "cosine"})
+        return collection
+    except Exception as ex:
+        logger.error(f"Error in creating a collection: {ex}")
+
+
+def add_to_collection(text_chunks, collection):
+    try:
+        documents = []
+        ids = []
+
+        for idx, text in enumerate(text_chunks):
+            random_id = str(uuid.uuid4())
+            ids.append(f"chunk_id{idx}_unique_id_{random_id}")
+            documents.append(text.pages_content)
+
+        collection.add(
+            documents=documents,
+            ids=ids
         )
-
-        response = qa_chain.run(user_question)
-        return jsonify({"response": response})
-
-    except FileNotFoundError as e:
-        return jsonify({"error": str(e)}), 400
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    except Exception as ex:
+        logger.error(f"Error to add in collection: {ex}")
